f83af3cdb3a3d340173a50efc187b4d92a0d1d07
Garrett Miller <garrettralphmiller@gmail.com>
2017-02-12 17:44:42 -0500

    parent theme updated from gumby.css to bootstrap.css grid

--- functions.php
+++ functions.php
@@ -1,17 +1,14 @@
 <?php
 function my_theme_enqueue_styles() {
-
     $parent_style = 'fotografo-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
-    $gumby_style = 'gumby-style';
-
+    $bootstrap_style = 'fotografo-bootstrap-style';
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( get_template_directory_uri().'./css/gumby.css');
+    wp_enqueue_style( $bootstrap_style,get_template_directory_uri().'/css/bootstrap.css');
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style, $gumby_style),
+        array( $parent_style, $bootstrap_style),
         wp_get_theme()->get('Version')
     );
 }
-
 add_action ('wp_enqueue_scripts', 'my_theme_enqueue_styles');
 ?>
